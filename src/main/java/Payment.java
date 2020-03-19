5b432e9a8e0648467419a6e78ed3259969b15913
Enfilde <lukewoody322@gmail.com>
2020-03-19 21:15:27 +0200

    Implemented Payment constructor

--- src/main/java/Payment.java
+++ src/main/java/Payment.java
@@ -1,11 +1,53 @@
+import Exceptions.*;
+
 import java.time.*;
 
 public class Payment {
 
-   private Organization senderName,recipientName;
-   private int uniqueCode;
+   private Organization senderName,recieverName;
+   private String uniqueCode;
    private String paymentPurpose;
    private LocalDateTime paymentDateTime;
    private double paymentSum;
+
+   public Payment(String senderName, String recieverName, String uniqueCode, String paymentPurpose,LocalDateTime paymentDateTime, double paymentSum) throws OrganizationAndPaymentsException {
+
+      if(senderName.equals(recieverName)) throw new PaymentForItselfException();
+      if(uniqueCode.isEmpty()) throw new EmptyPaymentIdException();
+      if(paymentPurpose.isEmpty()) throw new EmptyPaymentPurposeException();
+      if(paymentDateTime.isAfter(LocalDateTime.now())) throw new InvalidPaymentTimeException();
+      if (paymentSum <= 0) throw new InvalidPaymentSumException();
+
+      this.uniqueCode = uniqueCode;
+      this.paymentPurpose = paymentPurpose;
+      this.paymentDateTime = paymentDateTime;
+      this.paymentSum = paymentSum;
+
+   }
+
+
+   public Organization getSenderName() {
+      return senderName;
+   }
+
+   public Organization getRecieverName() {
+      return recieverName;
+   }
+
+   public String getUniqueCode() {
+      return uniqueCode;
+   }
+
+   public String getPaymentPurpose() {
+      return paymentPurpose;
+   }
+
+   public LocalDateTime getPaymentDateTime() {
+      return paymentDateTime;
+   }
+
+   public double getPaymentSum() {
+      return paymentSum;
+   }
 
 }
