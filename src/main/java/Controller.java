17cfa2a997952046a875dfcbea9da3d5b8ab3062
Enfilde <lukewoody322@gmail.com>
2020-03-21 16:56:08 +0200

    Implemented Contoller methods

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,73 +1,199 @@
+import Exceptions.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Controller {
 
-    private ArrayList<Payment> payments;
-    private ArrayList<Organization> organizations;
 
-    public int getUniqueCode() {
-        return 0;
+    public enum Found {
+        FALSE,
+        TRUE,
+        HALFTRUE,
     }
 
-    public String getPaymentPurpose() {
-        return null;
+
+    private ArrayList<Payment> payments = new ArrayList<>();
+    private ArrayList<Organization> organizations = new ArrayList<>();
+
+
+    public void addPayment(String senderName, String recieverName, String uniqueCode, String paymentPurpose, LocalDateTime paymentDateTime, double paymentSum) throws OrganizationAndPaymentsException {
+        Found organizationFound = Found.FALSE;
+        Organization senderOrganization = null;
+        Organization recieverOrganization = null;
+        for (Payment x : payments) {
+            if (x.getUniqueCode().equals(uniqueCode)) throw new DuplicatePaymentException();
+        }
+        for (Organization x : organizations) {
+            if (x.getOrganizationName().equals(recieverName)) {
+                organizationFound = Found.HALFTRUE;
+                recieverOrganization = x;
+                x.setBalance(x.getBalance()+paymentSum);
+                break;
+            }
+        }
+        for (Organization x : organizations) {
+            if (x.getOrganizationName().equals(senderName) && organizationFound == Found.HALFTRUE) {
+                senderOrganization = x;
+                organizationFound = Found.TRUE;
+                x.setBalance(x.getBalance()-paymentSum);
+                break;
+            }
+        }
+        if (organizationFound != Found.TRUE) throw new OrganizationNotFoundException();
+        payments.add(new Payment(senderOrganization, recieverOrganization, uniqueCode, paymentPurpose, paymentDateTime, paymentSum));
     }
 
-    public LocalDateTime getPaymentDateTime() {
-        return null;
+
+    public String getPaymentPurpose(String paymentId) throws OrganizationAndPaymentsException {
+        String paymentPurpose = null;
+        boolean paymentFound = false;
+        for (Payment x : payments) {
+            if (x.getUniqueCode().equals(paymentId)) {
+                paymentPurpose = x.getPaymentPurpose();
+                paymentFound = true;
+            }
+        }
+        if (!paymentFound) throw new NotFoundPaymentException();
+        return paymentPurpose;
     }
 
-    public double getPaymentSum() {
-        return 0;
+
+    public LocalDateTime getPaymentDateTime(String paymentId) throws OrganizationAndPaymentsException {
+        LocalDateTime date = null;
+        boolean checker = false;
+        for (Payment x : payments) {
+            if (x.getUniqueCode().equals(paymentId)) {
+                date = x.getPaymentDateTime();
+                checker = true;
+            }
+        }
+        if (!checker) throw new NotFoundPaymentException();
+        return date;
     }
 
-    public String getSenderName() {
-        return null;
+
+    public double getPaymentSum(String paymentId) throws OrganizationAndPaymentsException {
+        double sum = 0;
+        boolean paymentFound = false;
+        for (Payment x : payments) {
+            if (x.getUniqueCode().equals(paymentId)) {
+                sum = x.getPaymentSum();
+                paymentFound = true;
+            }
+        }
+        if (!paymentFound) throw new NotFoundPaymentException();
+        return sum;
     }
 
-    public String getRecipientName() {
-        return null;
+
+    public String getSenderName(String paymentId) throws OrganizationAndPaymentsException {
+        String senderName = null;
+        boolean paymentFound = false;
+        for (Payment x : payments) {
+            if (x.getUniqueCode().equals(paymentId)) {
+                senderName = x.getSenderOrganization().getOrganizationName();
+                paymentFound = true;
+            }
+        }
+        if (!paymentFound) throw new NotFoundPaymentException();
+        return senderName;
     }
 
-    public void addPayment() {
+
+    public String getRecieverName(String paymentId) throws OrganizationAndPaymentsException {
+        String recieverName = null;
+        boolean paymentFound = false;
+        for (Payment x : payments) {
+            if (x.getUniqueCode().equals(paymentId)) {
+                recieverName = x.getRecieverOrganiaton().getOrganizationName();
+                paymentFound = true;
+            }
+        }
+        if (!paymentFound) throw new NotFoundPaymentException();
+        return recieverName;
+    }
+
+
+    public void addOrganization(String name, String registrationNumber, double initialBalance) throws OrganizationAndPaymentsException {
+        for (Organization x : organizations) {
+            if (x.getOrganizationName().equals(name)) throw new DuplicateOrganizationException();
+            else if (x.getRegistrationNumber().equals(registrationNumber)) throw new DuplicateNumberException();
+        }
+        organizations.add(new Organization(name, registrationNumber, initialBalance));
 
     }
 
-    public String getOrganizationName() {
-        return null;
+
+    public String getRegistrationNumber(String organizationName) throws OrganizationAndPaymentsException {
+        for (Organization x : organizations) {
+            if (x.getOrganizationName().equals(organizationName)) return x.getRegistrationNumber();
+        }
+        throw new OrganizationNotFoundException();
+
     }
 
-    public String getRegistrationNumber() {
-        return null;
-    }
 
-    public double getBalance() {
-        return 0.0;
-    }
-
-    public void addOrganization() {
-
+    public double getBalance(String organizationName) throws OrganizationAndPaymentsException {
+        for (Organization x : organizations) {
+            if (x.getOrganizationName().equals(organizationName)) return x.getBalance();
+        }
+        throw new OrganizationNotFoundException();
     }
 
 
     public String getIdOfBiggestPayment() {
-        return null;
+        double highestPayment = 0;
+        String paymentId = null;
+        for (Payment x : payments) {
+            if (x.getPaymentSum() > highestPayment) {
+                highestPayment = x.getPaymentSum();
+                paymentId = x.getUniqueCode();
+            }
+        }
+        return paymentId;
     }
 
-    public LocalDateTime getDateWithBiggestTotalPayments() {
-        return null;
+
+    public LocalDate getDateWithBiggestTotalPayments() {
+        double totalPayments = 0;
+        double biggestTotalPayment = 0;
+        LocalDate biggestTotalPaymentDate = null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        for(Payment x : payments) {
+            for(Payment y: payments){
+                if(formatter.format(x.getPaymentDateTime()).equals(formatter.format(y.getPaymentDateTime()))){
+                    totalPayments += x.getPaymentSum();
+                }
+            }
+            if(totalPayments > biggestTotalPayment) {
+                biggestTotalPayment = totalPayments;
+                biggestTotalPaymentDate = x.getPaymentDateTime().toLocalDate();
+            }
+            totalPayments = 0;
+        }
+        return biggestTotalPaymentDate;
     }
 
-    public ArrayList<Organization> getFinalBalances() {
-        return new ArrayList<>();
+
+    public HashMap<String, Double> getFinalBalances() {
+        HashMap<String, Double> currentBalanceMap = new HashMap<>();
+        for (Organization x : organizations) {
+            currentBalanceMap.put(x.getOrganizationName(), x.getBalance());
+        }
+        return currentBalanceMap;
     }
 
-    public ArrayList<Organization> getOrganizationsWithNegativeSaldo() {
-        return new ArrayList<>();
-    }
 
+    public ArrayList<String> getOrganizationsWithNegativeSaldo() {
+        ArrayList<String> negativeBalanceList = new ArrayList<>();
+        for (Organization x : organizations) {
+            if (x.getBalance() < 0) negativeBalanceList.add(x.getOrganizationName());
+        }
+        return negativeBalanceList;
+    }
 }
-
 
 
